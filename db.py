283fccd93a881ee176eefba66c0885771bcb1b6d
bigidulka <udincevb@gmail.com>
2024-04-15 01:35:00 +0300

    fix

--- db.py
+++ db.py
@@ -19,25 +19,28 @@ def add_or_update_user(user_id, telegram_login, selected_group):
     if user_info:
         recent_groups = user_info[2] or ""
         groups_list = recent_groups.split(",") if recent_groups else []
-        
-        # Удаляем все предыдущие вхождения этой группы в списке
-        groups_list = [group for group in groups_list if group != selected_group]
-        
-        # Добавляем выбранную группу в конец списка
-        groups_list.append(selected_group)
+
+        # Удаляем все предыдущие вхождения этой группы в списке, убеждаемся, что group не None
+        groups_list = [group for group in groups_list if group != selected_group and group is not None]
+
+        # Добавляем выбранную группу в конец списка, если она не None
+        if selected_group:
+            groups_list.append(selected_group)
         
         # Оставляем только последние три группы
         groups_list = groups_list[-3:]
-        
+
         recent_groups = ",".join(groups_list)
     else:
-        recent_groups = selected_group
+        # Если selected_group None, установим пустую строку
+        recent_groups = selected_group if selected_group else ""
 
     cursor.execute('''
     INSERT INTO users (id, telegram_login, recent_groups) VALUES (?, ?, ?)
     ON CONFLICT(id) DO UPDATE SET telegram_login = excluded.telegram_login, recent_groups = excluded.recent_groups
     ''', (user_id, telegram_login, recent_groups))
     conn.commit()
+
 
 def get_user(user_id):
     """Получение информации о пользователе по ID."""
