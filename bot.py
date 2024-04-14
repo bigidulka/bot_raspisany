8306f5f6cdee34e4bba0fc5086eabf66bb74d472
bigidulka <udincevb@gmail.com>
2024-04-15 02:02:04 +0300

    f

--- bot.py
+++ bot.py
@@ -273,7 +273,7 @@ def message_all_users(update: Update, context: CallbackContext):
 def main():
     global schedule_data
     schedule_data = load_schedule(SCHEDULE_FILE)
-    updater = Updater("6818826799:AAErun_Xz5pMFQYgwxLe1ubS-FXofrPrxh8", use_context=True)
+    updater = Updater("6668495629:AAGlmeOCtw9dQxSXr31UugK9bLGfsimw-Xg", use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start))
